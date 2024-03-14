d0e9f5b0d6d3753308ee19ebca5a330d92fd55a4
FelipeKojiM <felipekoji1@outlook.com>
2024-03-13 22:01:08 -0300

    Adicionando trabalhos feitos em sala de aula

---
+++ CalculoIMC/src/main/java/com/mycompany/projetodesktop/ProjetoDesktop.java
@@ -0,0 +1,12 @@
+package com.mycompany.projetodesktop;
+
+import java.awt.Frame;
+
+public class ProjetoDesktop {
+
+    public static void main(String[] args) {
+
+        new NewJFrame1().setVisible(true);
+
+    }
+}
