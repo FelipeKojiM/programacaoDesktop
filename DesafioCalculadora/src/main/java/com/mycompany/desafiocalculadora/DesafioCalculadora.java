87f927b7160cb82f8de82a7aa8089eb7b79569f4
FelipeKojiM <felipekoji1@outlook.com>
2024-03-13 22:25:26 -0300

    adicionando calculadora

---
+++ DesafioCalculadora/src/main/java/com/mycompany/desafiocalculadora/DesafioCalculadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.desafiocalculadora;
+
+/**
+ *
+ * @author HP
+ */
+public class DesafioCalculadora {
+
+    public static void main(String[] args) {
+        new Calculadora().setVisible(true);
+    }
+}
